40597659463efa1df5ecb3731adf81d4c922d16a
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-19 21:58:54 +0900

    noteQuery

--- src/clientState.ts
+++ src/clientState.ts
@@ -1,6 +1,14 @@
 export const defaults = {
-    notes: []
+    notes: [
+        {
+            __typename: "Note",
+            id: 1,
+            title: "First",
+            content: "Second"
+        }
+    ]
 }
+
 export const typeDefs = [
     `
     schema{
@@ -12,8 +20,8 @@ export const typeDefs = [
         note[id: Int!]: Note
     }
     type Mutation{
-        createNote(title: String!, content: String!)
-        editNote(id: String!, title: String!, content: String!)
+        createNote(title: String!, content: String!):Note
+        editNote(id: String!, title: String!, content: String!): Note
     }
     type Note{
         id: Int!
@@ -25,6 +33,10 @@ export const typeDefs = [
 
 export const resolvers = {
     Query: {
-        notes: () => true
+        note: (_, variables, { getCacheKey }) => {
+            const id = getCacheKey({ __typename: "Note", id: variables.id });
+            console.log(id);
+            return null;
+        }
     }
 }
