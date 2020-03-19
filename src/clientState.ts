0946dd963edd549d18c325860c920dbf0f7f688b
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-19 21:42:55 +0900

    Type Definitions for Offline Schema

--- src/clientState.ts
+++ src/clientState.ts
@@ -1,3 +1,30 @@
-export const defaults: any = {}
-export const resolvers: any = {}
-export const typeDefs: any = {}
+export const defaults = {
+    notes: []
+}
+export const typeDefs = [
+    `
+    schema{
+        query: Query
+        mutation: Mutation
+    }
+    type Query{
+        notes: [Note]!
+        note[id: Int!]: Note
+    }
+    type Mutation{
+        createNote(title: String!, content: String!)
+        editNote(id: String!, title: String!, content: String!)
+    }
+    type Note{
+        id: Int!
+        title: String!
+        content: String!
+    }
+    `
+]
+
+export const resolvers = {
+    Query: {
+        notes: () => true
+    }
+}
