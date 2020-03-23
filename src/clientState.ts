db2c131882da560f0b8e16417e3dc6a232154f73
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-23 18:45:53 +0900

    localStorage

--- src/clientState.ts
+++ src/clientState.ts
@@ -1,21 +1,9 @@
 import { NOTE_FRAGMENT } from "./fragments";
 import { GET_NOTES } from "./queries";
+import { saveNotes } from "./offline";
 
 export const defaults = {
-    notes: [
-        {
-            __typename: "Note",
-            id: 1,
-            title: "First",
-            content: "- Second"
-        },
-        {
-            __typename: "Note",
-            id: 2,
-            title: "Second",
-            content: "- Third"
-        }
-    ]
+    notes: []
 }
 
 export const typeDefs = [
@@ -66,6 +54,7 @@ export const resolvers = {
                     notes: [newNote, ...notes]
                 }
             });
+            saveNotes(cache);
             return newNote;
         },
         editNote: (_, { id, title, content }, { cache }) => {
@@ -84,6 +73,7 @@ export const resolvers = {
                 fragment: NOTE_FRAGMENT,
                 data: updatedNote
             });
+            saveNotes(cache);
             return updatedNote;
         }
     }
